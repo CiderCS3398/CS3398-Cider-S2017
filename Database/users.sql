c22c2d1bb96ee74049d0a78c825a6ae111d2ddd9
ejd30 <ejd30@txstate.edu>
2017-04-10 17:29:29 -0500

    Comment mistake
    Sherman didn't realize making a comment on sql is different than other
    languages.

--- Database/users.sql
+++ Database/users.sql
@@ -1,11 +1,11 @@
-# A user will have their ID for quick reference in the database,
-# along with their username and password to log in. Additionally,
-# they should provide an email in case they forget their login
-# credentials.
-# A user might also be an admin, if we decide to give special
-# permissions within the app itself.
+--A user will have their ID for quick reference in the database,
+--along with their username and password to log in. Additionally,
+--they should provide an email in case they forget their login
+--credentials.
+--A user might also be an admin, if we decide to give special
+--permissions within the app itself.
 CREATE TABLE Users (
-# should we use UserID or UserName as key?
+--should we use UserID or UserName as key?
 UserID INT(5) NOT NULL AUTO_INCREMENT,
 UserName VARCHAR(15) NOT NULL,
 Password VARCHAR(20) NOT NULL,
@@ -14,9 +14,9 @@ AdminStatus BOOLEAN DEFAULT FALSE,
 PRIMARY KEY (UserID)
 );
 
-# A user will have several rows associated with their inventory
-# These rows will list the food (FoNo) along with the amount they
-# have of it, and when it expires.
+--A user will have several rows associated with their inventory
+--These rows will list the food (FoNo) along with the amount they
+--have of it, and when it expires.
 CREATE TABLE UserInventory (
 UserID NUMBER(5),
 FoNo NUMBER(5),
