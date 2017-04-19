1702a55ffbe1eef7227bd66fd43e7bea8c85d295
nla19 <nla19@txstate.edu>
2017-04-19 15:57:33 -0500

    Adding Recipe Class

---
+++ Research_and_Practice/Recipe.java
@@ -0,0 +1,13 @@
+/*
+  Nick's stab at refactoring the recipe class. Adapted to read recipes and
+  ingredients from database rather than json.
+*/
+
+import java.util.ArrayList;
+import java.sql.*;
+
+public class Recipe {
+  public class Ingredients{
+
+  }
+}
