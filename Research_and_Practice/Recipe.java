2137c5d552a6b33ed590d6442604e99900bdf4b7
Sherman <stt19@txstate.edu>
2017-04-25 16:45:52 -0500

    Revert "Merge remote-tracking branch 'origin/Develop' into Develop"
    This reverts commit b04fd0821f5be9aba4d5db19a9ae4a2d84902d6b, reversing
    changes made to a7c9a475e633fca4d8dc6de8cd896609e523ac4d.

--- Research_and_Practice/Recipe.java
+++ Research_and_Practice/Recipe.java
@@ -1,65 +1,13 @@
 /*
-    Nick's stab at refactoring Recipe class.
- */
-
-package com.example.android.wearable.recipeassistant;
-
-import android.content.Context;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+  Nick's stab at refactoring the recipe class. Adapted to read recipes and
+  ingredients from database rather than json.
+*/
 
 import java.util.ArrayList;
+import java.sql.*;
 
 public class Recipe {
-    private static final String TAG = "RecipeAssistant";
+  public class Ingredients{
 
-    public String titleText;
-    public String summaryText;
-    public String ingredientsText;
-
-    public Recipe() {
-        recipeSteps = new ArrayList<RecipeStep>();
-    }
-
-    public static Recipe fromJson(Context context, JSONObject json) {
-        Recipe recipe = new Recipe();
-        try {
-            recipe.titleText = json.getString(Constants.RECIPE_FIELD_TITLE);
-            recipe.summaryText = json.getString(Constants.RECIPE_FIELD_SUMMARY);
-
-            JSONArray ingredients = json.getJSONArray(Constants.RECIPE_FIELD_INGREDIENTS);
-            recipe.ingredientsText = "";
-            for (int i = 0; i < ingredients.length(); i++) {
-                recipe.ingredientsText += " - "
-                        + ingredients.getJSONObject(i).getString(Constants.RECIPE_FIELD_TEXT) + "\n";
-            }
-
-            }
-        } catch (JSONException e) {
-            Log.e(TAG, "Error loading recipe: " + e);
-            return null;
-        }
-        return recipe;
-    }
-
-    public Bundle toBundle() {
-        Bundle bundle = new Bundle();
-        bundle.putString(Constants.RECIPE_FIELD_TITLE, titleText);
-        bundle.putString(Constants.RECIPE_FIELD_SUMMARY, summaryText);
-        bundle.putString(Constants.RECIPE_FIELD_INGREDIENTS, ingredientsText);
-        return bundle;
-    }
-
-    public static Recipe fromBundle(Bundle bundle) {
-        Recipe recipe = new Recipe();
-        recipe.titleText = bundle.getString(Constants.RECIPE_FIELD_TITLE);
-        recipe.summaryText = bundle.getString(Constants.RECIPE_FIELD_SUMMARY);
-        recipe.ingredientsText = bundle.getString(Constants.RECIPE_FIELD_INGREDIENTS);
-        return recipe;
-    }
+  }
 }
