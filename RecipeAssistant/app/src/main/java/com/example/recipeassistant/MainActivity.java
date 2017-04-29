8c073328aa1aa7978898e9722b76fdc36c02a214
forrest87 <fs1051@txstate.edu>
2017-04-28 19:27:47 -0500

    Moving MainActivity with ListRecipes Button to Develop
    This was on my branch but needs to be on develop.

--- RecipeAssistant/app/src/main/java/com/example/recipeassistant/MainActivity.java
+++ RecipeAssistant/app/src/main/java/com/example/recipeassistant/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.recipeassistant;
 
-import android.support.v7.app.AppCompatActivity;
+//web ref: http://www.informit.com/articles/article.aspx?p=1646053&seqNum=3
+
+package cs3398_cider_s2017.kitchenhelper;
+
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MenuScreenActivity extends ListActivity
+{
+    private static final String APP_NAME = "CiderKitchenHelper";
+    private RecipeListAdapter mAdapter;
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(android.R.layout.custom_list_activity_view);
+        mAdapter = new RecipeListAdapter(this);
+        setListAdapter(mAdapter);
+        
+        //set up the button listener for listing recipes
+        Button listRecipesButton = (Button) findViewById(R.id.trigger);
+        listRecipesButton.setOnClickListener(new View.OnClickListener()
+                                             {
+            // This allows us to launch an activity.
+            // The appropriate Activity class will be called uing an
+            // Intent object and then by running the startActivity method.
+            public void onClick(ListView lv, int position)
+            {
+                String itemName = mAdapter.getItemName(position);
+                Intent intent = new Intent(getApplicationContext(),
+                                           ListRecipesActivity.class);
+                intent.putExtra(Constrants.RECIPE_NAME_TO_LOAD, itemName);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
