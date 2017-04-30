41bd70d8cb871c627ce6c2c08fd09a4410b5afec
Forrest <fs1051@txstate.edu>
2017-04-30 12:41:17 -0500

    For subsequent sprint development

---
+++ RecipeAssistant/app/src/main/java/com/example/recipeassistant/ListRecipesActivity.java
@@ -0,0 +1,73 @@
+package com.example.recipeassistant;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflator;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+public class ListRecipesActivity extends Activity
+{
+    private static final String TAG = "CiderKitchenHelper";
+    private String mRecipeName;
+    private Recipe mRecipe;
+    private TextView mTitleView;
+    private TextView mSummaryTextView;
+    private TextView mIngredientsTextView;
+    
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Intent intent = getIntent();
+        mRecipeName = "BBQ Pulled Pork"
+        displayRecipe();
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe);
+        mTitleView = (TextView) findViewById(R.id.recipeTextTitle);
+        mSummaryTextView = (TextView) findViewById(R.id.recipeTextSummary);
+        mIngredientsTextView = (TextView) findViewById(R.id.textIngredients);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it's present.
+        getMenuInflator().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    private void displayRecipe(Recipe recipe)
+    {
+        // set fade-in animation for a recipe name
+        Animation fadeIn = AnimationUtils.loadAnimation(this,
+                                                        android.R.anim.fade_in);
+        
+        // set an animation to the title TextView
+        mTitleTextView.setAnimation(fadeIn);
+        
+        // populate title, summary, and ingredients textviews with text
+        mTitleText.setText(recipe.titleText);
+        mSummaryTextView.setText(recipe.summaryText);
+        mIngredientsTextView.setText(recipe.ingredientsText);
+        
+        //
+        findViewByID(R.id.ingredientsHeader).setAnimation(fadeIn);
+        findViewByID(R.id.ingredientsHeader).setVisibility(View.VISIBLE);
+    }
+}
